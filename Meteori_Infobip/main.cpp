d054dcae517f4d69e6786f14f1d31ab4f4636238
Viktor Kulić Golja <viksi2806@gmail.com>
2020-03-30 16:59:36 +0200

    Testo

--- Meteori_Infobip/main.cpp
+++ Meteori_Infobip/main.cpp
@@ -1,12 +1,6 @@
-#include "pch.h"
-#include <iostream>
-#include <vector>
-#include "Meteor.h"
-#include "DataCollectorDevice.h"
-
+#include<iostream>
+#include"meteor_template.h"
 int main()
 {
-	std::vector<Meteor> v;
-	DataCollectorDevice dcd;
-	dcd.load_SonotaCo_data_to(v);
+
 }
